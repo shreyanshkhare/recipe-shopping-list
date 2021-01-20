82c782d19210ab66a74c88a3daa5f1e74a8b08a1
Anurag Jain <anuragjain979@gmail.com>
2021-01-20 21:33:37 +0530

    css changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,28 +4,29 @@ import { AuthGuard } from "./auth/auth-guard";
 import { AuthComponent } from "./auth/auth.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component"
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component"
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    {path: 'recipes', component: RecipesComponent,
-      canActivate: [AuthGuard],
-      children: [
-          {path: '', component: RecipeStartComponent},
-          {path: 'addNewRecipe', component: RecipeEditComponent},
-          {path: ':id', component: RecipeDetailComponent},
-          {path: ':id/edit', component: RecipeEditComponent}
+    { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+    {
+        path: 'recipes', component: RecipesComponent,
+        canActivate: [AuthGuard],
+        children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'addNewRecipe', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent },
+            { path: ':id/edit', component: RecipeEditComponent }
         ]
     },
-    {path: 'shopping-list', component: ShoppingListComponent},
-    {path: 'auth', component: AuthComponent}
+    { path: 'shopping-list', component: ShoppingListComponent },
+    { path: 'auth', component: AuthComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
     exports: [RouterModule]
 })
 
-export class AppRoutingModule {};
+export class AppRoutingModule { };
