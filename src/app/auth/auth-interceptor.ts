9172af33c4e283df49e2df3666384da08444971a
shreyansh <khareanshu3@gmail.com>
2021-01-25 17:29:51 +0530

    firebase changes

--- src/app/auth/auth-interceptor.ts
+++ src/app/auth/auth-interceptor.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import {environment} from 'src/environments/environment'
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         // return next.handle(req);
+        let baseUrl = environment.appBaseUrl+req.url        
         if (['/recipe/login/', '/recipe/user/'].includes(req.url)) {
-            return next.handle(req);
+            return next.handle(req.clone({
+                url:baseUrl,
+            }));
         }
 
         const token = JSON.parse(localStorage.getItem('userData') || '{}').access_token
         const modifyReq = req.clone({
+            url:baseUrl,
             setHeaders: {
                 'Cache-Control': 'no-cache',
                 'Pragma': 'no-cache',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  appBaseUrl:"https://premchalmeti.com"
 };
